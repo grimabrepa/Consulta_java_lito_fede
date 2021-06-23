afbfb9dd992d21c489bb006a853f3d3d9e19309a
Marcelo Peralta <ezcajo@yahoo.com.ar>
2021-06-22 23:29:05 -0300

    Consulat de lito y fede

---
+++ src/com/dominio/Principal.java
@@ -0,0 +1,22 @@
+
+package com.dominio;
+
+import com.dominio.gui.Gui;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Gui ventana = new Gui();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+    }
+    
+}
